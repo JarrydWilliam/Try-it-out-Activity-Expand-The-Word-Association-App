02f7414336a29894a7763a1cefc490d3b6844272
JarrydWilliam <134390764+JarrydWilliam@users.noreply.github.com>
2025-07-20 14:03:20 -0600

    Base UI running — added client-requested fields to registration page

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var loginUserRouter = require('./routes/loginuser');
+var registerRouter = require('./routes/register');
+var surveyRouter = require('./routes/survey');
+var timingsRouter = require('./routes/timings');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap-icons/")));
+
+app.use('/', indexRouter);
+app.use('/loginuser', loginUserRouter);
+app.use('/register', registerRouter);
+app.use('/survey', surveyRouter);
+app.use('/timings', timingsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/register.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET page. */
+router.get('/', function(req, res, next) {
+    console.log("register.js: GET");
+    res.render('register', { });
+});
+
+/* POST page. */
+router.post('/', function(req, res, next) {
+    
+    console.log("register.js: POST");
+    res.direct('/');
+});
+
+module.exports = router;
